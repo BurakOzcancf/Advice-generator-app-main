a64ff2206a8f55c67bf87a253ee0707e75b5d5e1
Burak Ozcan <burakozcancf@gmail.com>
2022-02-24 23:09:09 +0100

    Challenge first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let adviceNumber = document.querySelector(".advice__ID")
+let adviceDescription = document.querySelector(".advice__content")
+let adviceDice = document.querySelector(".advice__random")
+
+fetch('	https://api.adviceslip.com/advice')    
+    .then(response => response.json())
+    .then(data => {
+        adviceNumber.innerHTML = data.slip.id
+        adviceDescription.innerHTML = data.slip.advice
+    })
+     
+function updateAdvice() {
+    fetch('	https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+            adviceNumber.innerHTML = data.slip.id
+            adviceDescription.innerHTML = data.slip.advice
+        })
+        .catch(adviceDescription.innerHTML = "It may take some time")
+} 
+  

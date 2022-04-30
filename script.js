85739366a9a2f2ee72adc87deab48c5ee842d429
unknown <burakozcancf@gmail.com>
2022-05-01 00:24:03 +0200

    shortened

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ let adviceNumber = document.querySelector(".advice__ID")
 let adviceDescription = document.querySelector(".advice__content")
 let adviceDice = document.querySelector(".advice__random")
 
-fetch('	https://api.adviceslip.com/advice')    
-    .then(response => response.json())
-    .then(data => {
-        adviceNumber.innerHTML = data.slip.id
-        adviceDescription.innerHTML = data.slip.advice
-    })
-     
+window.addEventListener('load', () => {
+    updateAdvice()
+})
+
 function updateAdvice() {
     fetch('	https://api.adviceslip.com/advice')
         .then(response => response.json())
